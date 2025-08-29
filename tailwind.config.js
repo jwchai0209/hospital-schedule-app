6ba6624e27c21b0151624b6ec6af72e68d5354cf
jwchailocal <cjwoo9425@gmail.com>
2025-08-30 00:58:19 +0900

    Fix Tailwind CSS version and configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
+ /** @type {import('tailwindcss').Config} */
+export default {
+  content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
   ],
   theme: {
     extend: {},
