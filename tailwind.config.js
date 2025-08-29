e6773612140451b023fe33e191d2e2c2ad90ada8
jwchailocal <cjwoo9425@gmail.com>
2025-08-30 01:01:12 +0900

    Reset and reconfigure Tailwind CSS

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
- /** @type {import('tailwindcss').Config} */
-export default {
-  content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
   ],
   theme: {
     extend: {},
